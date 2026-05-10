944015c4992d3f259b54d465134f54077a46ba87
Jenny295 <76103544+Jenny295@users.noreply.github.com>
2026-05-10 22:46:46 +0200

    Update index.js

---
+++ Interfaces_Programm/Interface_tangible/Interface/www/resources/js/index.js
@@ -0,0 +1,93 @@
+
+
+import "./tab1.js";
+import "./tab2.js";
+import "./notifications.js";
+import { finishExperiment, downloadCSV } from "./logger.js";
+
+import { startQRScanner, stopQRScanner } from "./scanner.js";
+
+
+const buttons = document.querySelectorAll(".tab-button");
+const doneButton = document.getElementById("doneButton");
+
+let inputsDone = false;
+let notificationsDone = false;
+let tab2Done = false;
+let tab3Done = false;
+
+
+startQRScanner();
+
+doneButton.addEventListener("click", () => {
+    stopQRScanner();
+    finishExperiment();
+    downloadCSV();
+});
+
+
+// tracks states
+
+window.addEventListener("inputsFinished", () => {
+    inputsDone = true;
+    checkDone();
+});
+
+window.addEventListener("notificationsFinished", () => {
+    notificationsDone = true;
+    checkDone();
+});
+
+window.addEventListener("tab2Finished", () => {
+    tab2Done = true;
+    checkDone();
+});
+
+window.addEventListener("tab3TaskDone", () => {
+    tab3Done = true;
+    checkDone();
+});
+
+
+// checks if all tasks are completed and enables the done button.
+
+function checkDone() {
+
+    if (inputsDone && notificationsDone && tab2Done && tab3Done) {
+        doneButton.disabled = false;
+        doneButton.classList.add("enabled");
+    } else {
+        doneButton.disabled = true;
+        doneButton.classList.remove("enabled");
+    }
+}
+
+
+// global overlay to inform about new event in tab2 and pop up message
+
+window.addEventListener("tab2Start", () => {
+    showOverlay("Neue Aufgabe in Tab2 - sofort überprüfen.");
+});
+
+function showOverlay(PopUpText) {
+
+    const overlay = document.createElement("div");
+    overlay.id = "global-notification";
+
+    const box = document.createElement("div");
+    box.classList.add("box");
+
+    const text = document.createElement("p");
+    text.textContent = PopUpText;
+
+    const btn = document.createElement("button");
+    btn.textContent = "OK";
+
+    btn.onclick = () => overlay.remove();
+
+    box.appendChild(text);
+    box.appendChild(btn);
+    overlay.appendChild(box);
+
+    document.body.appendChild(overlay);
+}
